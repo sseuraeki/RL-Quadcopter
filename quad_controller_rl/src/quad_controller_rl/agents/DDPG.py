8831ed3160fc3e8701b8c87d5bb24c7870d5114c
Ren <sseuraeki@gmail.com>
2018-02-26 13:36:08 +0900

    ddpg fix

--- quad_controller_rl/src/quad_controller_rl/agents/DDPG.py
+++ quad_controller_rl/src/quad_controller_rl/agents/DDPG.py
@@ -11,15 +11,7 @@ from quad_controller_rl import util
 class DDPG(BaseAgent):
     """Reinforcement Learning agent that learns using DDPG."""
     def __init__(self, task):
-        # Task information
-        self.task = task
-        self.state_size = np.prod(self.task.observation_space.shape)
-        #self.state_size = 1
-        self.state_range = self.task.observation_space.high - self.task.observation_space.low
-        self.action_size = np.prod(self.task.action_space.shape)
-        #self.action_size = 1
-        self.action_range = self.task.action_space.high - self.task.action_space.low
-
+        ...
         # Actor (Policy) Model
         self.action_low = self.task.action_space.low
         self.action_high = self.task.action_space.high
@@ -45,73 +37,28 @@ class DDPG(BaseAgent):
         # Algorithm parameters
         self.gamma = 0.99  # discount factor
         self.tau = 0.001  # for soft update of target parameters
-
-        # writing episode stats
-        self.stats_filename = os.path.join(
-            util.get_param('out'),
-            "stats_{}.csv".format(util.get_timestamp()))  # path to CSV file
-        self.stats_columns = ['episode', 'total_reward']  # specify columns to save
-        self.episode_num = 1
-        print("Saving stats {} to {}".format(self.stats_columns, self.stats_filename))  # [debug]
-
-        # Episode variables
-        self.reset_episode_vars()
-
-    def reset_episode_vars(self):
-        self.last_state = None
-        self.last_action = None
-        self.total_reward = 0.0
-
-    def write_stats(self, stats):
-        """Write single episode stats to CSV file."""
-        df_stats = pd.DataFrame([stats], columns=self.stats_columns)  # single-row dataframe
-        df_stats.to_csv(self.stats_filename, mode='a', index=False,
-            header=not os.path.isfile(self.stats_filename))  # write header first time only
+        ...
 
     def step(self, state, reward, done):
-        # Transform state vector
-        state = (state - self.task.observation_space.low) / self.state_range  # scale to [0.0, 1.0]
-        state = state.reshape(1, -1)  # convert to row vector
-
-        # get only the z value
-        #state = state[:, 2]
-        #state = state.reshape(-1, 1)
-
+        ...
         # Choose an action
         action = self.act(state)
 
         # Save experience / reward
         if self.last_state is not None and self.last_action is not None:
             self.memory.add(self.last_state, self.last_action, reward, state, done)
-            self.total_reward += reward
-
+        ...
         # Learn, if enough samples are available in memory
         if len(self.memory) > self.batch_size:
             experiences = self.memory.sample(self.batch_size)
             self.learn(experiences)
-
-        self.last_state = state
-        self.last_action = action[:,2] # add linear z only
-
-        if done:
-            # Write episode stats
-            print("Total reward: {}".format(self.total_reward)) # debug
-            self.write_stats([self.episode_num, self.total_reward])
-            self.episode_num += 1
-            self.reset_episode_vars()
-
-        return action
+        ...
 
     def act(self, states):
         """Returns actions for given state(s) as per current policy."""
         states = np.reshape(states, [-1, self.state_size])
         actions = self.actor_local.model.predict(states)
-        actions += self.noise.sample()  # add some noise for exploration
-
-        # get only the z linear force
-        #actions = np.array([[0., 0., actions, 0., 0., 0.]])
-
-        return actions
+        return actions + self.noise.sample()  # add some noise for exploration
 
     def learn(self, experiences):
         """Update policy and value parameters using given batch of experience tuples."""
