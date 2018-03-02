f3bbf7cc1cdbec7290dc81f2848d297b814dbfca
Ren <sseuraeki@gmail.com>
2018-03-02 09:01:10 +0900

    hover fix

--- quad_controller_rl/src/quad_controller_rl/tasks/hover.py
+++ quad_controller_rl/src/quad_controller_rl/tasks/hover.py
@@ -1,4 +1,4 @@
-"""Takeoff task."""
+"""Hover task."""
 
 import numpy as np
 from gym import spaces
@@ -6,9 +6,12 @@ from geometry_msgs.msg import Vector3, Point, Quaternion, Pose, Twist, Wrench
 from quad_controller_rl.tasks.base_task import BaseTask
 
 class Hover(BaseTask):
-    """the goal is to stay at 10 units height for a few seconds"""
+    """Simple task where the goal is to lift off the ground and reach a target height."""
 
     def __init__(self):
+        # task name for saving models
+        self.taskname = 'hover'
+
         # State space: <position_x, .._y, .._z, orientation_x, .._y, .._z, .._w>
         cube_size = 300.0  # env is cube_size x cube_size x cube_size
         self.observation_space = spaces.Box(
@@ -25,17 +28,13 @@ class Hover(BaseTask):
         #print("Takeoff(): action_space = {}".format(self.action_space))  # [debug]
 
         # Task-specific parameters
-        self.max_duration = 10.0  # secs
+        self.max_duration = 5.0  # secs
         self.target_z = 10.0  # target height (z position) to reach for successful takeoff
 
     def reset(self):
-        # reset last conditions needed
-        self.last_timestamp = None
-        self.last_position = None
-
-        # return initial condition
+        # Nothing to reset; just return initial condition
         return Pose(
-                position=Point(0.0, 0.0, np.random.normal(10.0, 0.1)),  # drop off from somewhere after takeoff
+                position=Point(0.0, 0.0, np.random.normal(10.0, 0.1)),  # drop off from the situation after takeoff
                 orientation=Quaternion(0.0, 0.0, 0.0, 0.0),
             ), Twist(
                 linear=Vector3(0.0, 0.0, 0.0),
@@ -48,34 +47,10 @@ class Hover(BaseTask):
                 pose.position.x, pose.position.y, pose.position.z,
                 pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w])
 
-        # compute and add velocity to states
-        if self.last_timestamp is None:
-            velocity = 0.
-        else:
-            velocity = abs(pose.position.z - self.last_position) / \
-                       max(timestamp - self.last_timestamp, 1e-6) # 1e-6 to avoid zero division
-
         # Compute reward / penalty and check if this episode is complete
         done = False
-        reward = 0.0
-        reward += -min(abs(self.target_z - pose.position.z), 20.0)  # the farther away from the target z, the less reward
-        reward += timestamp # the longer the copter survives, the more reward
-        reward += -velocity # the faster velocity the less reward
-
-        # update needed states
-        self.last_timestamp = timestamp
-        self.last_position = pose.position.z
-
-        # define done conditions
-        if pose.position.z < 1.0:
-            reward -= 100.0 # big penalty when it hits the ground (or very close to it)
-            done = True
-        elif pose.position.z > self.target_z + 10.0:
-            reward -= 100.0 # big penalty when it goes too high up
-            done = True
-        elif timestamp > self.max_duration:  # task end
-            # big bonus when it survives the target duration
-            reward += 100.0
+        reward = -(abs(self.target_z - pose.position.z), 20.0)  # reward = zero for matching target z, -ve as you go farther, upto -20
+        if timestamp > self.max_duration:  # task done
             done = True
 
         # Take one RL step, passing in current state and reward, and obtain action
