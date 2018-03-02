8975a04999261ac07b1c7c6123e525812f5a89b8
Ren <sseuraeki@gmail.com>
2018-03-02 10:54:55 +0900

    init fix

--- quad_controller_rl/src/quad_controller_rl/tasks/__init__.py
+++ quad_controller_rl/src/quad_controller_rl/tasks/__init__.py
@@ -2,3 +2,4 @@ from quad_controller_rl.tasks.base_task import BaseTask
 from quad_controller_rl.tasks.takeoff import Takeoff
 from quad_controller_rl.tasks.hover import Hover
 from quad_controller_rl.tasks.landing import Landing
+from quad_controller_rl.tasks.landing import Combined
